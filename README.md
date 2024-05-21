2d0a95e1e9649a96d50a1965768610d1385605cd
gornius <ninertominer@gmail.com>
2024-05-21 19:37:14 +0200

    Implementacja

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bot Krzysztofa Bosaka przepisany na GO
+![Sposób działania](.github/czat.png)
+
+## Uruchamianie
+W zmiennych środowiskowych zapisać do zmiennej `KRZYSZTOF_GOSAK_TOKEN` token bota Discorda, np.
+```
+export KRZYSZTOF_GOSAK_TOKEN=my_discord_bot_token
+```
+
+Uruchomić program (skompilować lub za pomocą `go run .`)
--- main.go
+++ main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	authToken := os.Getenv("KRZYSZTOF_GOSAK_TOKEN")
+	dg, err := discordgo.New("Bot " + authToken)
+	if err != nil {
+		panic(err)
+	}
+
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer dg.Close()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(m.Content), "można") {
+		s.ChannelMessageSendReply(m.ChannelID, "Można. Gdyby to było złe to Bóg by inaczej świat stworzył.", m.Reference())
+	}
+}
